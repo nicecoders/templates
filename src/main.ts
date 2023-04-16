c8cc60a90f6f18b1f0712db7f8677fd2c11fd3bf
dev <710328466@qq.com>
2023-04-16 16:15:18 +0800

    feat: 添加vue-ant-design

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css';
 import router from './router'
 
 import './assets/main.css'
@@ -9,6 +10,7 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Antd)
 app.use(router)
 
 app.mount('#app')
